c3594d1481929c6765db48c0d21dc0046cf02474
pcordeir <pcordeir@student.42.de>
2022-08-04 13:41:46 +0200

    started parsing

--- check_file.c
+++ check_file.c
@@ -6,7 +6,7 @@
 /*   By: pcordeir <pcordeir@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 11:24:17 by pcordeir          #+#    #+#             */
-/*   Updated: 2022/08/04 12:39:19 by pcordeir         ###   ########.fr       */
+/*   Updated: 2022/08/04 13:40:54 by pcordeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,6 @@
 int	checkfile(char *path)
 {
 	int	fd;
-	// t_obj	*obj;
-
-	// obj = malloc(sizeof(t_obj));
 
 	fd = open(path, O_RDONLY);
 	if (fd == -1)
@@ -26,13 +23,12 @@ int	checkfile(char *path)
 		return (-1);
 	}
 	return (readfile(fd));
-
-	return (0);	
 }
 
 int	readfile(int fd)
 {
 	char	*line;
+	int		err;
 
 	line = get_next_line(fd);
 	if (!line)
@@ -40,15 +36,49 @@ int	readfile(int fd)
 		printf("File empty!\n");
 		return (-1);
 	}
+	err = 0;
 	while (line)
 	{
-		printf("Line: %s\n", line);
+		if (*line != '\n' && err != -1)
+		{
+			printf("Line: %s", line);
+			err = checkline(line);
+		}
 		free(line);
 		line = get_next_line(fd);
 	}
-	// close(fd);
+	close(fd);
 	return(0);
 }
+
+int	checkline(char *line)
+{
+	while (*line && (*line == ' ' || *line == '\t'))		// recognize tabs? 
+		line++;
+	if (!*line || *line == '\n')
+		return (0);
+	else if (*line == 65)
+		return (printf("Create amlight!\n"));
+		// return (create_amlight(line));
+	else if (*line == 67)
+		return (printf("Create camera!\n"));
+		// return (create_camera(line));
+	else if (*line == 76)
+		return (printf("Create light!\n"));
+		// return (create_light(line));
+	else if (*line == 115 && *(line + 1) == 112)
+		return (printf("Create sphere!\n"));
+		// return (create_sphere(line));
+	else if (*line == 112 && *(line + 1) == 108)
+		return (printf("Create plane!\n"));
+		// return (create_plane(line));
+	else if (*line == 99 && *(line + 1) == 121)
+		return (printf("Create cylinder!\n"));
+		// return (create_cylinder(line));
+	else
+		return (-1);
+}
+
 // t_obj	**obj;
 // obj = malloc(sizeof(t_obj *) * 5);
 // obj[0] = malloc(sizeof(t_obj));
--- mini_rt.h
+++ mini_rt.h
@@ -6,7 +6,7 @@
 /*   By: pcordeir <pcordeir@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 15:12:46 by chelmerd          #+#    #+#             */
-/*   Updated: 2022/08/04 12:23:46 by pcordeir         ###   ########.fr       */
+/*   Updated: 2022/08/04 13:15:31 by pcordeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,5 +102,6 @@ typedef struct s_obj {
 //	check_file.c
 int	checkfile(char *path);
 int	readfile(int fd);
+int	checkline(char *line);
 
 #endif
