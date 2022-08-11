26b70ae9718470f1bc76871580e12f21f8d82bc6
chelmerd <chelmerd@student.42wolfsburg.de>
2022-08-11 16:23:21 +0200

    add ft_minf to find the smaller float number

--- float_utils.c
+++ float_utils.c
@@ -6,13 +6,21 @@
 /*   By: chelmerd <chelmerd@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 11:43:58 by chelmerd          #+#    #+#             */
-/*   Updated: 2022/08/10 11:51:29 by chelmerd         ###   ########.fr       */
+/*   Updated: 2022/08/11 14:55:41 by chelmerd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 float	ft_maxf(float a, float b)
 {
 	if (a > b)
+		return (a);
+	else
+		return (b);
+}
+
+float	ft_minf(float a, float b)
+{
+	if (a < b)
 		return (a);
 	else
 		return (b);
--- mini_rt.h
+++ mini_rt.h
@@ -6,7 +6,7 @@
 /*   By: chelmerd <chelmerd@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 15:12:46 by chelmerd          #+#    #+#             */
-/*   Updated: 2022/08/10 15:55:44 by chelmerd         ###   ########.fr       */
+/*   Updated: 2022/08/11 16:22:38 by chelmerd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -208,6 +208,7 @@ void	*new(size_t size);
 
 // float_utils
 float	ft_maxf(float a, float b);
+float	ft_minf(float a, float b);
 
 // color
 
