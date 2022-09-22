93ec0c5eac524235a6175ba3a72fb91fbd7831fd
chelmerd <chelmerd@student.42wolfsburg.de>
2022-09-22 14:52:31 +0200

    add extra function for creating the camera matrix

--- camera.c
+++ camera.c
@@ -6,20 +6,23 @@
 /*   By: chelmerd <chelmerd@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 12:21:19 by chelmerd          #+#    #+#             */
-/*   Updated: 2022/09/22 11:22:06 by chelmerd         ###   ########.fr       */
+/*   Updated: 2022/09/22 14:51:21 by chelmerd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_rt.h"
 
+static
+void	create_camera_matrix(
+			t_camera *camera,
+			float pos[VEC3_SIZE],
+			float normal[VEC3_SIZE],
+			float v_up[VEC3_SIZE]);
+
 t_camera	*new_camera(float pos[VEC3_SIZE], float ovector[VEC3_SIZE], float fov)
 {
 	t_camera	*camera;
-	struct		s_mat4 transf;
-	float		v_up[VEC3_SIZE]; // where is up in the world
-	float		v[VEC3_SIZE]; // camera vertical
-	float		normal[VEC3_SIZE]; // view plane normal, i. e. camera orientation
-	float		u[VEC3_SIZE]; // right
+	float 		v_up[VEC3_SIZE]; // where is up in the world?
 
 	camera = new(sizeof(t_camera));
 	if (!camera)
@@ -33,16 +36,22 @@ t_camera	*new_camera(float pos[VEC3_SIZE], float ovector[VEC3_SIZE], float fov)
 	camera->ovector[2] = ovector[2];
 	camera->fov = fov;
 
-	vec3(0,1,0, v_up);
-	vec3_copy(ovector, normal);
-	// cross product of parrallel vectors is not helpfull
-	if (normal[1] == 1.f || normal[1] == -1.f)
-		vec3(1,0,0, v_up);
+	if (ovector[1] == 1.f || ovector[1] == -1.f)
+		vec3(1, 0, 0, v_up);
+	create_camera_matrix(camera, pos, ovector, vec3(0, 1, 0, v_up));
 
-	// init v, n, u
+	return (camera);
+}
+
+static
+void	create_camera_matrix(t_camera *camera, float pos[VEC3_SIZE], float normal[VEC3_SIZE], float v_up[VEC3_SIZE])
+{
+	struct		s_mat4 transf;
+	float		v[VEC3_SIZE]; // camera vertical
+	float		u[VEC3_SIZE]; // right
+
 	vec3_cross(v_up, normal, u);
 	vec3_cross(normal, u, v);
-
 	transf.m11 = u[0];
 	transf.m21 = u[1];
 	transf.m31 = u[2];
@@ -52,7 +61,6 @@ t_camera	*new_camera(float pos[VEC3_SIZE], float ovector[VEC3_SIZE], float fov)
 	transf.m13 = -normal[0];
 	transf.m23 = -normal[1];
 	transf.m33 = -normal[2];
-
 	transf.m14 = pos[0];
 	transf.m24 = pos[1];
 	transf.m34 = pos[2];
@@ -60,9 +68,6 @@ t_camera	*new_camera(float pos[VEC3_SIZE], float ovector[VEC3_SIZE], float fov)
 	transf.m41 = 0;
 	transf.m42 = 0;
 	transf.m43 = 0;
-
 	mat_copy(transf.v, MAT4_SIZE, camera->transform.forward);
 	mat4_inverse(camera->transform.forward, camera->transform.backward);
-
-	return (camera);
 }
