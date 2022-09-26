278418fe1d40e7faa0291f684cc8667e93b43499
chelmerd <chelmerd@student.42wolfsburg.de>
2022-09-26 14:46:10 +0200

    create camera matrix by using translate_rotate()

--- camera.c
+++ camera.c
@@ -6,14 +6,11 @@
 /*   By: chelmerd <chelmerd@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 12:21:19 by chelmerd          #+#    #+#             */
-/*   Updated: 2022/09/23 15:24:50 by chelmerd         ###   ########.fr       */
+/*   Updated: 2022/09/26 14:44:54 by chelmerd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_rt.h"
-
-static	void	create_camera_matrix(t_camera *camera, float pos[VEC3_SIZE], \
-				float normal[VEC3_SIZE], float v_up[VEC3_SIZE]);
 
 t_camera	*new_camera(float pos[VEC3_SIZE], float ovector[VEC3_SIZE], \
 			float fov)
@@ -35,35 +32,6 @@ t_camera	*new_camera(float pos[VEC3_SIZE], float ovector[VEC3_SIZE], \
 	vec3(0, 1, 0, v_up);
 	if (ovector[1] == 1.f || ovector[1] == -1.f)
 		vec3(1, 0, 0, v_up);
-	create_camera_matrix(camera, pos, ovector, v_up);
+	translate_rotate(pos, ovector, v_up, &camera->transform);
 	return (camera);
-}
-
-static	void	create_camera_matrix(t_camera *camera, float pos[VEC3_SIZE], \
-				float normal[VEC3_SIZE], float v_up[VEC3_SIZE])
-{
-	struct s_mat4	transf;
-	float			v[VEC3_SIZE];
-	float			u[VEC3_SIZE];
-
-	vec3_cross(normal, v_up, u);
-	vec3_cross(u, normal, v);
-	transf.m11 = u[0];
-	transf.m21 = u[1];
-	transf.m31 = u[2];
-	transf.m12 = v[0];
-	transf.m22 = v[1];
-	transf.m32 = v[2];
-	transf.m13 = -normal[0];
-	transf.m23 = -normal[1];
-	transf.m33 = -normal[2];
-	transf.m14 = pos[0];
-	transf.m24 = pos[1];
-	transf.m34 = pos[2];
-	transf.m44 = 1;
-	transf.m41 = 0;
-	transf.m42 = 0;
-	transf.m43 = 0;
-	mat_copy(transf.v, MAT4_SIZE, camera->transform.forward);
-	mat4_inverse(camera->transform.forward, camera->transform.backward);
 }
